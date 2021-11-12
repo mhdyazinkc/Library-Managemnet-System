46cb8e76e0f4e5f116a9acda5444767ecb9f921a
MUHAMMED YASEEN K C <ymkcmpd@gmail.com>
2021-11-12 09:18:37 +0530

    Update 1.5.5

---
+++ build/classes/library2/Alogin.rs
@@ -0,0 +1,5 @@
+Slibrary.Alogin$1
+Slibrary.Alogin$3
+Slibrary.Alogin$2
+Slibrary.Alogin$4
+Slibrary.Alogin
---
+++ src/library2/Library2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library2;
+
+/**
+ *
+ * @author munshi
+ */
+public class Library2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
